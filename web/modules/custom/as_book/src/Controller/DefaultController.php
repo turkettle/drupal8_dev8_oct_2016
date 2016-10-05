2269290c38de3a2806e5552d0fa3eba3f849269d
Aston Lille <admin@aston-lille.fr>
2016-10-05 15:15:53 +0200

    Remise à niveau.

--- web/modules/custom/as_book/src/Controller/DefaultController.php
+++ web/modules/custom/as_book/src/Controller/DefaultController.php
@@ -12,21 +12,21 @@ use Drupal\Core\Controller\ControllerBase;
 class DefaultController extends ControllerBase {
 
   /**
-   * Listing.
+   * Booklisting.
    *
    * @return string
    *   Return Hello string.
    */
-  public function listing() {
+  public function bookListing() {
 
     $query = \Drupal::entityQuery('node');
-    $query->condition('type', 'book', '=');
+    $query->condition('type', 'as_book');
     $query->condition('status', 1);
+    $query->sort('created', 'DESC');
     $query->range(0, 10);
-    $query->sort('created', 'ASC');
-    $nids = $query->execute();
+    $result = $query->execute();
 
-    $nodes = \Drupal\node\Entity\Node::loadMultiple($nids);
+    $nodes = \Drupal\node\Entity\Node::loadMultiple($result);
 
     $books = [];
     foreach ($nodes as $node) {
@@ -34,10 +34,12 @@ class DefaultController extends ControllerBase {
     }
 
     return [
-      '#theme' => 'book_list',
+      '#theme' => 'book_listing',
       'books' => $books,
     ];
   }
+
+
 
 
 
